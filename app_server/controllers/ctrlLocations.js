2a2b6982057249fafa04d2d4b91c056f91844f18
Pavel Sergeyev <xblxTurtle@gmail.com>
2018-08-23 13:27:47 +0100

    Move hardcoded data from Location view into controller

--- app_server/controllers/ctrlLocations.js
+++ app_server/controllers/ctrlLocations.js
@@ -27,7 +27,30 @@ let homeList = function (req,resp,next) {
 }
 
 let locationInfo = function (req,resp,next) {
-    resp.render('location', {title: 'Location'})
+    resp.render('location', {
+        title: 'Location',
+        name: "Honest Burger",
+        rating: 3,
+        distance: 100,
+        address: "125 High street, Reading, RG6 1PS",
+        facilities: ["toilet", "tables", "heating"],
+        hours: ["Monday - Friday : 07:00am 07:00pm", "Saturday : 08:00am 05:00pm", "Sunday : Closed"],
+        reviews: [
+            {
+                rating: 5,
+                name: "John Malkovich",
+                date: "13/01/2018",
+                text: "This is the best place ever!"
+            },
+            {
+                rating: 2,
+                name: "Jack Rambo",
+                date: "01/10/2018",
+                text: "Nice place. Food is not ideal, though."
+            }
+        ],
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
+    })
 }
 
 let addReview =  function (req,resp,next) {
